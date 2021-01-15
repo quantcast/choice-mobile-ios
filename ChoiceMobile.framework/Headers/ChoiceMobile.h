6d6b30b8a31b21396094905eeb16551a846bfc83
Kevin Smith <kevin_smith@quantcast.com>
2021-01-15 13:17:26 -0800

    initial upload of framework

---
+++ ChoiceMobile.framework/Headers/ChoiceMobile.h
@@ -0,0 +1,18 @@
+//
+//  ChoiceMobile.h
+//  ChoiceMobile
+//
+//  Copyright © 2020 Quantcast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChoiceMobile.
+FOUNDATION_EXPORT double ChoiceMobileVersionNumber;
+
+//! Project version string for ChoiceMobile.
+FOUNDATION_EXPORT const unsigned char ChoiceMobileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChoiceMobile/PublicHeader.h>
+
+
